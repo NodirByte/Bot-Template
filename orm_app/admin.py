2495807a029c2cb69409ea174cfb09b004f478f2
dyrhq <160723074+dyrhq@users.noreply.github.com>
2024-04-21 12:57:12 +0500

    feat: initial setup of project and db schema

--- orm_app/admin.py
+++ orm_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from orm_app.models import (
+    Category,
+    Container,
+    Organization,
+    Product,
+    User,
+    Sale,
+    Review,
+    SaleReview,
+)
+
+
+admin.site.register(Category)
+admin.site.register(Container)
+admin.site.register(Organization)
+admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Sale)
+admin.site.register(Review)
+admin.site.register(SaleReview)
--- orm_app/models.py
+++ orm_app/models.py
@@ -1,3 +1,121 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    telegram_id = models.IntegerField()
+    phone_number = models.CharField(max_length=100, null=False, blank=False)
+    organization = models.ForeignKey(
+        "Organization", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    number = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    organization = models.ForeignKey(
+        "Organization", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Container(models.Model):
+    number = models.CharField(max_length=100)
+    arrival_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.number
+
+
+class Sale(models.Model):
+    user = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    container = models.ForeignKey(
+        "Container", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    purchase_date = models.DateField()
+    review_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class Review(models.Model):
+    EXCELLENT = "EXCELLENT"
+    MEDIUM = "MEDIUM"
+    BAD = "BAD"
+    RATING_CHOICES = [
+        (EXCELLENT, "Excellent"),
+        (MEDIUM, "Medium"),
+        (BAD, "Bad"),
+    ]
+    user = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    rating = models.CharField(max_length=100, choices=RATING_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.rating
+
+
+class SaleReview(models.Model):
+    sale = models.ForeignKey("Sale", on_delete=models.SET_NULL, null=True, blank=True)
+    review = models.ForeignKey(
+        "Review", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.sale.product.name + " " + self.review.rating
